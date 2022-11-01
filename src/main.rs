12f8460dce9b06192656118040dcf92a315827b1
lambdatamer <lambdatamer@gmail.com>
2022-11-01 20:59:50 +0200

    Extract program logic into run function

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,13 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents = fs::read_to_string(config.file_path).expect("Failed to read the file");
 
-    println!("With text: \n{contents}");
+    println!("With text:\n {contents}");
 }
 
 struct Config {
