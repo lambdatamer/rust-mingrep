2ac26a18ad13614ce69b0b7b1d56c61dbea63cc2
lambdatamer <lambdatamer@gmail.com>
2022-11-01 19:54:02 +0200

    Read text from a file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,6 +6,10 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Failed to read the file");
+
+    println!("With text: \n, {contents}");
 }
